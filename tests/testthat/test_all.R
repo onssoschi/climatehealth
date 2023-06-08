0a54590f1b3ba9d2ea536e005a1c36fbbd1f8545
Euan Soutter <redacted+euan-soutter@users.noreply.github.com>
2023-06-08 08:54:02 +0100

    WTCHF-239: add successful test

---
+++ tests/testthat/test_all.R
@@ -0,0 +1,12 @@
+library(testthat)
+library(indicatorfunctions)
+
+test_that('Test new output matches original output', {
+
+  expected_output <- read.csv('testdata/output_one_region_data_original.csv')
+
+  actual_output <- read.csv('testdata/output_one_region_data_new.csv')
+
+  expect_equal(actual_output, expected_output)
+
+})
