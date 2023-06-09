2d263344428e39a36b1a37f23b423c8b2743dcb2
Euan Soutter <redacted+euan-soutter@users.noreply.github.com>
2023-06-09 14:23:18 +0100

    WTCHF-231: add attrdl func, fix tests

--- tests/testthat/test_all.R
+++ tests/testthat/test_all.R
@@ -3,6 +3,8 @@ library(indicatorfunctions)
 
 test_that('Test new output matches original output', {
 
+  do_analysis('testdata/regEngWales.csv', output_csv_path = 'testdata/output_one_region_data_new.csv')
+
   expected_output <- read.csv('testdata/output_one_region_data_original.csv')
 
   actual_output <- read.csv('testdata/output_one_region_data_new.csv')
