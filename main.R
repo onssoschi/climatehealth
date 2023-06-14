42d2001bdeb5d62dc5c2cc8bcc9a1c21cab6ced8
soutte <euan.soutter@ons.gov.uk>
2023-06-14 11:08:38 +0100

    WTCHF-240: edit main file, edit output paths

--- main.R
+++ main.R
@@ -1,2 +1,31 @@
-# Main script to call in modules
-do_analysis()
+library(dlnm)
+library(mvmeta)
+library(splines)
+library(tsModel)
+library(config)
+library(zeallot)
+
+# 1. Load parameters from config file --------------------------------------------------
+
+# Load config file
+config <- config::get()
+
+# Input data
+input_csv_path <- config$input_csv_path
+
+# Output data
+output_csv_path <- config$output_csv_path
+
+# Specification of the exposure function
+varfun <- config$varfun
+vardegree <- config$vardegree
+varper <- c(10,75,90)
+
+# Specification of the lag function
+lag <- config$lag
+lagnk <- config$lagnk
+
+# Degree of freedom for seasonality
+dfseas <- config$dfseas
+
+# 1. Load parameters from config file --------------------------------------------------
