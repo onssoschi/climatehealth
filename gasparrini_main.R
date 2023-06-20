43556eb654caed3877447aaa0a181bb1e11b6eec
soutte <euan.soutter@ons.gov.uk>
2023-06-20 10:57:55 +0100

    WTCHF-245: add main file

---
+++ gasparrini_main.R
@@ -0,0 +1,15 @@
+devtools::load_all()
+library(dlnm)
+library(mvmeta)
+library(splines)
+library(tsModel)
+library(config)
+library(zeallot)
+
+###############################################################################
+# IMPORTANT: Before running this script update the following in config.yml:   #
+#             input_csv_path [input dataset]                                  #
+#             output_folder_path [folder that stores data and figure outputs] #
+###############################################################################
+
+do_analysis(config$input_csv_path, config$output_folder_path)
