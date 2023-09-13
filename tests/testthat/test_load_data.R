5f2b6f001e3ac11ce7ec2dbe1b94c988b64e4981
Antony Brown <redacted+antony-brown@users.noreply.github.com>
2023-09-13 15:04:12 +0100

    Update test_load_data.R (new test for list order)

--- tests/testthat/test_load_data.R
+++ tests/testthat/test_load_data.R
@@ -27,3 +27,28 @@ test_that('Test data loads correctly', {
 
 })
 
+test_that('Test regions sorted correctly', {
+
+  config <- config::get()
+
+  c(df_list_) %<-%
+    load_data(
+      input_csv_path = config$input_csv_path,
+      dependent_col = config$dependent_col,
+      time_col = config$time_col,
+      region_col = config$region_col,
+      temp_col = config$temp_col,
+      time_range = config$time_range,
+      region_names = config$region_names
+    )
+
+  config_names_random <- sample(config$region_names)
+
+  config_names_ordered <- sort(config_names_random)
+
+  df_list_names <- names(df_list_)
+
+  expect_equal(df_list_names, config_names_ordered)
+
+})
+
