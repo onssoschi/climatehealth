71e14cea10c3310c1eda53e7d0e3cd33b40e96f1
Browning <Charlie.Browning@ons.gov.uk>
2024-10-11 15:19:35 +0100

    feat: update and add docstrings

--- R/defence_utils.R
+++ R/defence_utils.R
@@ -5,6 +5,16 @@
 # TODO: explore how to make this function more dynamic (take a type as an arg)
 
 
+#' Check that a passed argument is a list of dataframes.
+#'
+#' @param list_ A list of items.
+#' @param raise Whether or not to raise an error.
+#' @param param_nm The name of the parameter that the list was originally passed
+#' to.
+#'
+#' @return Whether or not the item passed (list_) is a list of dfs.
+#' @export
+#'
 is_list_of_dfs <- function(list_,
                            raise = TRUE,
                            param_nm = "df_list") {
@@ -30,6 +40,6 @@ is_list_of_dfs <- function(list_,
     if (raise) {
       stop(paste(param_nm, " expected a list. Got ", toString(typeof(list_)), sep = ""))
     }
-      }
+  }
   return (invalid)
 }
