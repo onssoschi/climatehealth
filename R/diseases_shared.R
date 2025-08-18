f730d5bb833931b66c972042b91fc7f27b001e6a
CBROWN-ONS <charlie.browning@ons.gov.uk>
2025-08-18 13:34:30 +0100

    feat: follow best practice to avoid warnings

--- R/diseases_shared.R
+++ R/diseases_shared.R
@@ -1072,7 +1072,7 @@ contour_plot <- function(
     filled.contour(
       x, y, z,
       xlab = "Lag",
-      ylab = ifelse(param_term == "tmax", "Temperature (°C)",
+      ylab = ifelse(param_term == "tmax", "Temperature (\u00b0C)",
                     ifelse(param_term == "rainfall", "Rainfall (mm)", param_term)),
       main = title,
       col = cols,
