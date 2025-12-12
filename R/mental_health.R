1219854fa27a22e2ace3c7bdabe8c1520ef77c3b
Kenechi Omeke <kenechi.omeke@ons.gov.uk>
2025-12-12 09:33:26 +0000

    Revert "Merge branch 'dev' of https://github.com/onssoschi/climatehealth into dev"
    This reverts commit c7f31681044662ecfba82b7625f2e2fe5d03a052, reversing
    changes made to df55aa0ec44c977fda4e8bdbec3bfa1ecc6b0690.

--- R/mental_health.R
+++ R/mental_health.R
@@ -47,17 +47,8 @@ mh_read_and_format_data <- function(
     health_outcome_col,
     population_col
   )
-  standard_cols <- c(
-    "date", "region", "tmean", "health_outcome", "population"
-  )
-  for (i in seq_along(standard_cols)) {
-    std_col <- standard_cols[i]
-    need_col <- needed_cols[i]
-    if (!identical(std_col, need_col) && std_col %in% names(df)) {
-      df[[std_col]] <- NULL
-    }
-  }
   df <- df %>%
+    dplyr::select(all_of(needed_cols)) %>%
     dplyr::rename(
       date = date_col,
       region = region_col,
