65158896e5d706cf8b0c7ecf59d79d3803d2c121
Scarbrough <Felix.Scarbrough@ons.gov.uk>
2026-02-10 12:22:59 +0000

    Adds R/globals.R to register tidyverse/ggplot NSE variables via utils::globalVariables(), following R CMD check guidance and best practice for tidy evaluation.

---
+++ R/globals.R
@@ -0,0 +1,12 @@
+utils::globalVariables(c(
+  "year", "month", "district", "geometry", "value", "group", "lon", "lat",
+  "AR_Fraction", "avg", "label_text",
+  "x", "y", "ymin", "ymax",
+  "RR", "RR_low", "RR_high",
+  "climate_scaled", "dev.list",
+  "region",
+  # ggplot2 functions
+  "ggplot", "aes", "geom_col", "geom_text", "scale_y_continuous",
+  "labs", "position_dodge", "theme", "theme_minimal", "element_text", "margin",
+  "scale_fill_manual", "scale_fill_brewer"
+))
