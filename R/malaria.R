f4523791a2889b62d8caa7541f54866d9fa8afef
Scarbrough <redacted+scarbrough@users.noreply.github.com>
2026-02-11 16:36:04 +0000

    restored output folder functionality for malaria.R and diarrhea.R

--- R/malaria.R
+++ R/malaria.R
@@ -126,6 +126,21 @@ malaria_do_analysis <- function(health_data_path,
     stop("'output_dir' must be provided is 'save_fig' or save_csv' are TRUE.")
   }
   check_file_exists(output_dir, TRUE)
+  if (!is.null(output_dir)) {
+    # Check output dir exists
+    check_file_exists(output_dir, TRUE)
+    # Create a centralised output dir
+    new_fpath <- file.path(
+      output_dir,
+      paste0("malaria_analysis_", format(Sys.time(), "%d_%m_%Y_%H_%M"))
+    )
+    if (!is.null(new_fpath)) {
+      (
+        dir.create(new_fpath)
+      )
+    }
+    output_dir <- new_fpath
+  }
 
   # level validation
   level <- tolower(level)
