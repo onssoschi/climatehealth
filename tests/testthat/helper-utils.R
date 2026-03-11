acefc560a840db9e685ad83bb657e15231234e56
Kenechi Omeke <122579668+kenechiomeke-ONS@users.noreply.github.com>
2026-03-11 12:06:35 +0000

    suppress_plot()tracks specific open devices to close only those

--- tests/testthat/helper-utils.R
+++ tests/testthat/helper-utils.R
@@ -1,6 +1,12 @@
 suppress_plot <- function(expr) {
   tmp <- tempfile(fileext = ".pdf")
-  pdf(tmp, width = 12, height = 9)
-  on.exit(dev.off(), add = TRUE)
+  grDevices::pdf(tmp, width = 12, height = 9)
+  plot_dev <- grDevices::dev.cur()
+  on.exit({
+    open_devices <- grDevices::dev.list()
+    if (!is.null(open_devices) && plot_dev %in% open_devices) {
+      grDevices::dev.off(which = plot_dev)
+    }
+  }, add = TRUE)
   force(expr)
 }
